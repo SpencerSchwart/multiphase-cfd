ca9bf8ca95b3a8307ca812dfc13a6f897f2a8e15
SpencerSchwart <spencer.scs.us@gmail.com>
2025-03-03 09:50:28 -0500

    fixed mistake in maxu calculation

--- project2/cpp/src/timestep.cpp
+++ project2/cpp/src/timestep.cpp
@@ -8,13 +8,13 @@ TODO: add feature to search for max value of mu
 
 void set_timestep (FaceVectorField& uf, double& dt, double dx, FaceVectorField& mu)
 {
-    double maxu = 0;
+    double maxu = -1e30;
     FOREACH()
     {
-        if (fabs(uf.x(i,j)) > maxu && uf.x(i,j) > 0) 
+        if (fabs(uf.x(i,j)) > maxu && fabs(uf.x(i,j)) > 0) 
             maxu = fabs(uf.x(i,j));
 
-        if (fabs(uf.y(i,j)) > maxu && uf.y(i,j) > 0) 
+        if (fabs(uf.y(i,j)) > maxu && fabs(uf.y(i,j)) > 0) 
             maxu = fabs(uf.y(i,j));
     }
     
